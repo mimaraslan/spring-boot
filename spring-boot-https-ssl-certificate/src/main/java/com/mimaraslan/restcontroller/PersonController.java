f13348560192e3551c9dd3df70e496a27b7c44ac
mimaraslan <mimaraslan@gmail.com>
2019-04-06 23:28:47 +0300

    HTTPS SSL Certificate
    Spring Boot Hypertext Transfer Protocol Secure (HTTPS)  SSL Certificate.

---
+++ spring-boot-https-ssl-certificate/src/main/java/com/mimaraslan/restcontroller/PersonController.java
@@ -0,0 +1,13 @@
+package com.mimaraslan.restcontroller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonController {
+	
+	@GetMapping(value="/welcome")
+	public String name() {
+		return "Hello HTTPS";
+	}
+}
