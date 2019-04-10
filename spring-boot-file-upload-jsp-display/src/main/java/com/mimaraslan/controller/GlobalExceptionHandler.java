66ed564ccfd221e2baaba227b2332d15e1e4218b
mimaraslan <mimaraslan@gmail.com>
2019-04-11 00:32:26 +0300

    Image file upload and display on the JSP page demo project for Spring Boot
    Image file upload and display on the JSP page demo project for Spring Boot

---
+++ spring-boot-file-upload-jsp-display/src/main/java/com/mimaraslan/controller/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.mimaraslan.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MultipartException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MultipartException.class)
+    public String handleError1(MultipartException e, RedirectAttributes redirectAttributes) {
+
+        redirectAttributes.addFlashAttribute("message", e.getCause().getMessage());
+        return "redirect:/uploadStatus";
+    }
+
+    /*@ExceptionHandler(MultipartException.class)
+    public String handleError2(MultipartException e) {
+        return "redirect:/errorPage";
+    }*/
+}
