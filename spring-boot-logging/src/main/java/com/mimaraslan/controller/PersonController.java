1a57dc96598506de2365a8a5acb27f2d426c7969
mimaraslan <mimaraslan@gmail.com>
2019-04-15 20:24:47 +0300

    SLF4J Logger demo project for Spring Boot
    SLF4J Logger demo project for Spring Boot

---
+++ spring-boot-logging/src/main/java/com/mimaraslan/controller/PersonController.java
@@ -0,0 +1,32 @@
+package com.mimaraslan.controller;
+
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.mimaraslan.model.Person;
+import com.mimaraslan.service.PersonService;
+
+@RestController
+public class PersonController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(PersonController.class);
+	
+	@Autowired
+	private PersonService personService;
+	
+	@GetMapping("/person")
+	public List<Person> getPersons() {
+		
+		String className = this.getClass().getSimpleName();
+        logger.trace("A TRACE Message: "+className);
+        logger.debug("A DEBUG Message: "+className);
+        logger.info("An INFO Message: "+className);
+        logger.warn("A WARN Message: "+className);
+        logger.error("An ERROR Message: "+className);
+         
+		return personService.getPersons();
+	}
+}
---
+++ spring-boot-logging/src/main/java/com/mimaraslan/service/PersonService.java
@@ -0,0 +1,30 @@
+package com.mimaraslan.service;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import com.mimaraslan.model.Person;
+
+@Service
+public class PersonService {
+	
+	private static final Logger logger = LoggerFactory.getLogger(PersonService.class);
+	public List<Person> getPersons(){
+		
+		String className = this.getClass().getSimpleName();
+        logger.trace("A TRACE Message: "+className);
+        logger.debug("A DEBUG Message: "+className);
+        logger.info("An INFO Message: "+className);
+        logger.warn("A WARN Message: "+className);
+        logger.error("An ERROR Message: "+className);
+        
+		return Arrays.asList(
+				new Person(1, "person1"),
+				new Person(2, "person2")
+				);
+	}
+}
