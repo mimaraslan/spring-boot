68e76063e63c47e55ad647230e7be9c4d247d3ee
Mimar Aslan <mimaraslan@gmail.com>
2019-04-18 18:05:22 +0300

    Lombok demo project for Spring Boot
    Lombok demo project for Spring Boot

---
+++ spring-boot-lombok/src/main/java/com/mimaraslan/controller/EmployeeController.java
@@ -0,0 +1,30 @@
+package com.mimaraslan.controller;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.mimaraslan.model.Employee;
+
+import lombok.Lombok;
+
+@Controller
+public class EmployeeController {
+
+	@GetMapping("/")
+	public String viewEmployee(Map<String, Object> model) {
+		
+//		Employee employee = new EmployeeBuilder()
+//                .name("Mimar Aslan")
+//                .address("Turkey")
+//                .age("38")
+//                .build();
+//
+//System.out.println(lombokModel);  
+		model.put("message", this.message);
+		return "index";
+	}
+	
+
+}
