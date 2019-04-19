2e1edd9fb529f7d2ac6a76e3a9dea04d9bbeb22d
Mimar Aslan <mimaraslan@gmail.com>
2019-04-19 11:43:03 +0300

    Spring Boot
    Spring Boot

--- spring-boot-lombok/src/main/java/com/mimaraslan/MainApplication.java
+++ spring-boot-lombok/src/main/java/com/mimaraslan/MainApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootLombokApplication {
+public class MainApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootLombokApplication.class, args);
+		SpringApplication.run(MainApplication.class, args);
 	}
 
 }
