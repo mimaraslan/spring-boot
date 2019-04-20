4fb887768d93db7f85a41d8cf2b62e5641b512c5
mimaraslan <mimaraslan@gmail.com>
2019-04-20 16:06:43 +0300

    Spring Boot
    Spring Boot

--- spring-boot-https-ssl-certificate/src/main/java/com/mimaraslan/MainApplication.java
+++ spring-boot-https-ssl-certificate/src/main/java/com/mimaraslan/MainApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class MainApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(MainApplication.class, args);
 	}
 
 }
