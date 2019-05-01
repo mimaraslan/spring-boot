cd247ba256fdbbcb4e8064492ae3942a1ed9f093
Mimar Aslan <mimaraslan@gmail.com>
2019-05-01 21:41:11 +0300

    Spring Boot

---
+++ spring-boot-aws-dynamodb/src/main/java/com/mimaraslan/model/Address.java
@@ -0,0 +1,68 @@
+package com.mimaraslan.model;
+
+import java.io.Serializable;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBDocument;
+
+@DynamoDBDocument
+public class Address implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@DynamoDBAttribute
+	private String addressLine1;
+
+	@DynamoDBAttribute
+	private String addressLine2;
+
+	@DynamoDBAttribute
+	private String state;
+
+	@DynamoDBAttribute
+	private String city;
+
+	@DynamoDBAttribute
+	private String zipCode;
+
+	public String getAddressLine1() {
+		return addressLine1;
+	}
+
+	public void setAddressLine1(String addressLine1) {
+		this.addressLine1 = addressLine1;
+	}
+
+	public String getAddressLine2() {
+		return addressLine2;
+	}
+
+	public void setAddressLine2(String addressLine2) {
+		this.addressLine2 = addressLine2;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+}
---
+++ spring-boot-aws-dynamodb/src/main/java/com/mimaraslan/repository/DynamoDbRepository.java
@@ -0,0 +1,56 @@
+package com.mimaraslan.repository;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBSaveExpression;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.ComparisonOperator;
+import com.amazonaws.services.dynamodbv2.model.ConditionalCheckFailedException;
+import com.amazonaws.services.dynamodbv2.model.ExpectedAttributeValue;
+import com.mimaraslan.model.Student;
+
+@Repository
+public class DynamoDbRepository {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(DynamoDbRepository.class);
+
+	@Autowired
+	private DynamoDBMapper mapper;
+
+	public Student insertIntoDynamoDB(Student student) {
+		mapper.save(student);
+		return mapper.load(Student.class, student.getStudentId(), student.getLastName());
+	}
+
+	public Student getOneStudentDetails(String studentId, String lastName) {
+		return mapper.load(Student.class, studentId, lastName);
+	}
+	
+	public Student updateStudentDetails(Student student) {
+		try {
+			mapper.save(student, buildDynamoDBSaveExpression(student));
+			return mapper.load(Student.class, student.getStudentId(), student.getLastName());
+		} catch (ConditionalCheckFailedException exception) {
+			LOGGER.error("invalid data - " + exception.getMessage());
+			return null;
+		}
+	}
+
+	public void deleteStudentDetails(Student student) {
+		mapper.delete(student);
+	}
+
+	public DynamoDBSaveExpression buildDynamoDBSaveExpression(Student student) {
+		DynamoDBSaveExpression saveExpression = new DynamoDBSaveExpression();
+		Map<String, ExpectedAttributeValue> expected = new HashMap<>();
+		expected.put("studentId", new ExpectedAttributeValue(new AttributeValue(student.getStudentId()))
+				.withComparisonOperator(ComparisonOperator.EQ));
+		saveExpression.setExpected(expected);
+		return saveExpression;
+	}
+}
