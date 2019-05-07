0079b797f1eb99021e9eaa8a5d188b682f8715c6
Mimar Aslan <mimaraslan@gmail.com>
2019-05-07 13:27:42 +0300

    Spring Boot
    Spring Boot

--- spring-boot-aws-rds-crud/src/main/java/com/mimaraslan/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mimaraslan.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mimaraslan.model.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-}
--- spring-boot-aws-rds-crud/src/main/webapp/WEB-INF/jsp/includes/footer.jsp
+++
@@ -1,3 +0,0 @@
-	<script type="text/javascript" src="webjars/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</body>
-</html>
