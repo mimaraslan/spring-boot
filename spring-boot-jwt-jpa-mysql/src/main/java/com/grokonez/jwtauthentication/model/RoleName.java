dfaf3ac96321f7863c4679722b329e2f1f985d7d
Mimar Aslan <mimaraslan@gmail.com>
2019-05-13 18:04:21 +0300

    JWT
    JWT

---
+++ spring-boot-jwt-jpa-mysql/src/main/java/com/grokonez/jwtauthentication/model/RoleName.java
@@ -0,0 +1,7 @@
+package com.grokonez.jwtauthentication.model;
+
+public enum  RoleName {
+    ROLE_USER,
+    ROLE_PM,
+    ROLE_ADMIN
+}
---
+++ spring-boot-jwt-jpa-mysql/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO mydemodb.roles(name) VALUES('ROLE_USER');
+INSERT INTO mydemodb.roles(name) VALUES('ROLE_PM');
+INSERT INTO mydemodb.roles(name) VALUES('ROLE_ADMIN');
