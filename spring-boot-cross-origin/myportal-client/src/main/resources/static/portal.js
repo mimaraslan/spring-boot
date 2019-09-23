65df6821ec6d0a5dca9a3f6dcaa4134a70a47857
mimaraslan <mimaraslan@gmail.com>
2019-09-23 20:41:34 +0300

    Cross-Origin, Client - Server
    Cross-Origin, Client - Server

--- spring-boot-cross-origin/myportal-client/src/main/resources/static/portal.js
+++ spring-boot-cross-origin/myportal-client/src/main/resources/static/portal.js
@@ -1,19 +1,18 @@
-GET: $(document).ready(
-		function() {
+GET: $(document).ready(function() {
 
-			// GET REQUEST
-			$("#accessportal").click(function(event) {
-				event.preventDefault();
-				ajaxGet();
-			});
+	// GET REQUEST
+	$("#accessPortal").click(function(event) {
+		event.preventDefault();
+		ajaxGet();
+	});
 
-			// DO GET
-			function ajaxGet() {
-				$.ajax({
-					url : "http://localhost:8080/access",
-					success : function(result) {
-						$("#apiResponse").html(result);
-					}
-				});
+	// DO GET
+	function ajaxGet() {
+		$.ajax({
+			url : "http://localhost:8080/access",
+			success : function(result) {
+				$("#apiResponse").html(result);
 			}
-		})
+		});
+	}
+})
