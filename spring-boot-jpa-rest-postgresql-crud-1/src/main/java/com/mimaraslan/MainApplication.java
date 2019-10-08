6a69e959269159c0b5f0451b2415e7498e7f4975
mimaraslan <mimaraslan@gmail.com>
2019-10-08 21:57:30 +0300

    Spring Boot JPA Hibernate RESTful PostgreSQL CRUD
    Spring Boot JPA Hibernate RESTful PostgreSQL CRUD

--- spring-boot-jpa-rest-postgresql-crud-1/src/main/java/com/mimaraslan/MainApplication.java
+++ spring-boot-jpa-rest-postgresql-crud-1/src/main/java/com/mimaraslan/MainApplication.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
+@EnableJpaEAuditing
 @SpringBootApplication
-public class MaintApplication {
+public class MainApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MaintApplication.class, args);
+		SpringApplication.run(MainApplication.class, args);
 	}
 
 }
