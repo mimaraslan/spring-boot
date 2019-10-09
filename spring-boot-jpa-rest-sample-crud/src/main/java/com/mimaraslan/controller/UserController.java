069318d9619d8d9e95384678ed98fc3b95fabdcd
mimaraslan <mimaraslan@gmail.com>
2019-10-09 23:32:14 +0300

    Spring Boot JPA RESTful Simple CRUD
    Spring Boot JPA RESTful Simple CRUD

--- spring-boot-jpa-rest-sample-crud/src/main/java/com/mimaraslan/controller/UserController.java
+++ spring-boot-jpa-rest-sample-crud/src/main/java/com/mimaraslan/controller/UserController.java
@@ -4,13 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mimaraslan.model.User;
@@ -22,41 +22,60 @@ public class UserController {
 
 	@Autowired
 	private UserRepository userRepository;
+
 	
+	// http://localhost:8080/api/v1/users
 	@GetMapping("/users")
 	public List<User> getUserAll() {
 		return userRepository.findAll();
 	}
-	
+
+	// http://localhost:8080/api/v1/users/1
 	@GetMapping("/users/{id}")
-	public ResponseEntity<User> getUserFindById(Long id) {
-		User user = userRepository.findById(id).get();		
+	public ResponseEntity<User> getUserFindById(@PathVariable(value = "id") Long id) {
+		User user = userRepository.findById(id).get();
 		return ResponseEntity.ok().body(user);
 	}
+
 	
+	/* 
+	 http://localhost:8080/api/v1/users
+	
+	{
+		"firstName": "Mimar",
+		"lastName": "Aslan"
+	}
+	*/
 	@PostMapping("/users")
 	public User createUser(@RequestBody User user) {
 		return userRepository.save(user);
 	}
+
 	
-	@PutMapping("/users")
-	public ResponseEntity<User> updateUserById(User updateUser, Long id) {
+	/* 
+	 http://localhost:8080/api/v1/users/1
+	
+	{
+		"firstName": "Murat",
+		"lastName": "Aylin"
+	}
+	*/
+	@PutMapping("/users/{id}")
+	public ResponseEntity<User> updateUserById(@RequestBody User updateUser, @PathVariable Long id) {
 		User user = userRepository.findById(id).get();
 		user.setFirstName(updateUser.getFirstName());
-		user.setLastName(updateUser.getLastName());	
+		user.setLastName(updateUser.getLastName());
 		userRepository.save(user);
 		return ResponseEntity.ok().body(user);
 	}
-	
-	public String deleteUserById(Long id) {
-		User user = userRepository.findById(id).get();
 
-		if(!user.equals(null)) {
-			userRepository.delete(user);
-			return "OK";
-		}
-		return "ID not found.";
+	// http://localhost:8080/api/v1/users/1
+	@DeleteMapping("/users/{id}")
+	public String deleteUserById(@PathVariable(value = "id") Long id) {
+		User user = userRepository.findById(id).get();
+		userRepository.delete(user);
+		return "OK";
 
 	}
-	
+
 }
