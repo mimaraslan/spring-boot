d1f1f160c0858dfd4547edc765dc9d23b48fec89
Mimar Aslan <mimaraslan@gmail.com>
2019-10-15 09:58:15 +0300

    Spring Boot Docker Dockerize
    Spring Boot Docker Dockerize

--- spring-boot-dockerize/src/main/java/com/mimaraslan/controller/UserController.java
+++ spring-boot-dockerize/src/main/java/com/mimaraslan/controller/UserController.java
@@ -1,12 +1,14 @@
 package com.mimaraslan.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1")
 public class UserController {
 
-	// http://localhost:9090/message
+	// http://localhost:9090/api/v1/message
 	@GetMapping("/message")
 	public String getMessage() {
 		return "Welcome to Spring Boot with Docker";
