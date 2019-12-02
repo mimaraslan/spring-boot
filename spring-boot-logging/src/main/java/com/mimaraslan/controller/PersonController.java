94e5db01ba620ae4d02adf23d763d858e78d3a98
mimaraslan <mimaraslan@gmail.com>
2019-12-02 16:14:02 +0300

    Spring Boot - Log
    Spring Boot - Log

--- spring-boot-logging/src/main/java/com/mimaraslan/controller/PersonController.java
+++ spring-boot-logging/src/main/java/com/mimaraslan/controller/PersonController.java
@@ -11,22 +11,22 @@ import com.mimaraslan.service.PersonService;
 
 @RestController
 public class PersonController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(PersonController.class);
-	
+
 	@Autowired
 	private PersonService personService;
-	
+
 	@GetMapping("/person")
 	public List<Person> getPersons() {
-		
+
 		String className = this.getClass().getSimpleName();
-        logger.trace("A TRACE Message: "+className);
-        logger.debug("A DEBUG Message: "+className);
-        logger.info("An INFO Message: "+className);
-        logger.warn("A WARN Message: "+className);
-        logger.error("An ERROR Message: "+className);
-         
+		logger.trace("A TRACE Message: " + className);
+		logger.debug("A DEBUG Message: " + className);
+		logger.info("An INFO Message: " + className);
+		logger.warn("A WARN Message: " + className);
+		logger.error("An ERROR Message: " + className);
+
 		return personService.getPersons();
 	}
 }
--- spring-boot-logging/src/main/java/com/mimaraslan/service/PersonService.java
+++ spring-boot-logging/src/main/java/com/mimaraslan/service/PersonService.java
@@ -9,20 +9,18 @@ import com.mimaraslan.model.Person;
 
 @Service
 public class PersonService {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(PersonService.class);
-	public List<Person> getPersons(){
-		
+
+	public List<Person> getPersons() {
+
 		String className = this.getClass().getSimpleName();
-        logger.trace("A TRACE Message: "+className);
-        logger.debug("A DEBUG Message: "+className);
-        logger.info("An INFO Message: "+className);
-        logger.warn("A WARN Message: "+className);
-        logger.error("An ERROR Message: "+className);
-        
-		return Arrays.asList(
-				new Person(1, "person1"),
-				new Person(2, "person2")
-				);
+		logger.trace("A TRACE Message: " + className);
+		logger.debug("A DEBUG Message: " + className);
+		logger.info("An INFO Message: " + className);
+		logger.warn("A WARN Message: " + className);
+		logger.error("An ERROR Message: " + className);
+
+		return Arrays.asList(new Person(1, "person1"), new Person(2, "person2"));
 	}
 }
