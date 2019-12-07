13d37563d34c75c3f8f1c543cb29dfe00102e60e
mimaraslan <mimaraslan@gmail.com>
2019-12-07 15:50:19 +0300

    Spring Boot Apache Spark
    Spring Boot Apache Spark

--- spring-boot-spark/README.md
+++ spring-boot-spark/README.md
@@ -2,3 +2,18 @@
 Apache Spark’s classic Word Count example with Spring Boot. 
 
 ![Spring Boot + Apache Spark](src/main/resources/images/springboot+apachespark.png)
+
+
+OPEN
+http://localhost:8085/wordcount
+
+BODY - FORM DATA
+
+KEY 
+word
+
+VALUE
+Anya|Katerina|Anya|Svetlana|Lena|Rita|Lena|Katerina|Katerina
+
+![Spring Boot + Apache Spark](src/main/resources/readme-wordcount.png)
+
--- spring-boot-spark/src/main/java/com/mimaraslan/service/WordCountService.java
+++ spring-boot-spark/src/main/java/com/mimaraslan/service/WordCountService.java
@@ -1,12 +1,12 @@
 package com.mimaraslan.service;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Map;
 
 @Service
 public class WordCountService {
