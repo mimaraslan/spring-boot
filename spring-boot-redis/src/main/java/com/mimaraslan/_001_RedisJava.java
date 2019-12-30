6c650426273e20ca9caca90709287e19d1a3c975
mimaraslan <mimaraslan@gmail.com>
2019-12-30 12:10:31 +0300

    Spring Boot Redis
    Spring Boot Redis

--- spring-boot-redis/src/main/java/com/mimaraslan/_001_RedisJava.java
+++ spring-boot-redis/src/main/java/com/mimaraslan/_001_RedisJava.java
@@ -4,6 +4,7 @@ import redis.clients.jedis.Jedis;
 
 public class _001_RedisJava {
 	public static void main(String[] args) {
+		
 		// Connecting to Redis server on localhost
 		Jedis jedis = new Jedis("localhost");
 		System.out.println("Connection to server sucessfully");
