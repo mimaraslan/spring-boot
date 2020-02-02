b3721f2672f0cbb83d9e9da7269e6be02f937b20
mimaraslan <mimaraslan@gmail.com>
2020-02-03 02:38:12 +0300

    Spring Boot Docker
    Spring Boot Docker

--- spring-boot-dockerize/src/main/java/com/mimaraslan/controller/DockerDemoController.java
+++ spring-boot-dockerize/src/main/java/com/mimaraslan/controller/DockerDemoController.java
@@ -1,4 +1,4 @@
-package com.mimaraslan.demo.controller;
+package com.mimaraslan.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
