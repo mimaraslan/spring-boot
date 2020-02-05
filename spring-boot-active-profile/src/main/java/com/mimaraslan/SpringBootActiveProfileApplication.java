4454388112d0897233ff4ecbfafb29f62aaac129
mimaraslan <mimaraslan@gmail.com>
2020-02-05 22:41:30 +0300

    Spring Boot Active Profile Application
    Spring Boot Active Profile Application

---
+++ spring-boot-active-profile/src/main/java/com/mimaraslan/SpringBootActiveProfileApplication.java
@@ -0,0 +1,13 @@
+package com.mimaraslan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootActiveProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootActiveProfileApplication.class, args);
+	}
+
+}
