efe1dd16cbca0ad980c7393df55066a807365bc0
mimaraslan <mimaraslan@gmail.com>
2020-08-05 21:55:43 +0300

    Spring Boot Bean Autowired
    Spring Boot Bean Autowired

--- spring-boot-bean-autowired/src/main/java/com/mimaraslan/config/MyInterface.java
+++ spring-boot-bean-autowired/src/main/java/com/mimaraslan/config/MyInterface.java
@@ -1,6 +1,9 @@
 package com.mimaraslan.config;
 
 public interface MyInterface {
-    public String getAppName();    
+	
+    public String getAppName();  
+    
     public int getAppPortNumber(); 
+    
 }
