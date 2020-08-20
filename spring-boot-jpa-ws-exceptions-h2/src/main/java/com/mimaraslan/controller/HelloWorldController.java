a7164fc4c56e2a8870543511f17849ecbdd45353
mimaraslan <mimaraslan@gmail.com>
2020-08-21 01:33:32 +0300

    Spring Boot JPA WS Exceptions H2
    Path variable

--- spring-boot-jpa-ws-exceptions-h2/src/main/java/com/mimaraslan/controller/HelloWorldController.java
+++ spring-boot-jpa-ws-exceptions-h2/src/main/java/com/mimaraslan/controller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.mimaraslan.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,5 +16,10 @@ public class HelloWorldController {
 	@GetMapping("/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean ("Hello World");
+	}
+	
+	@GetMapping("/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldBean(@PathVariable String name) {
+		return new HelloWorldBean ("Hello World " + name);
 	}
 }
