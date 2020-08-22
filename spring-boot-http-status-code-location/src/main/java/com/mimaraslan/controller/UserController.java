f4d46b5fafe2bbff275f56e3822fd54d928eac85
mimaraslan <mimaraslan@gmail.com>
2020-08-23 02:57:28 +0300

    Spring Boot HTTP Status Code Location
    Spring Boot HTTP Status Code Location

--- spring-boot-http-status-code-location/src/main/java/com/mimaraslan/controller/UserController.java
+++ spring-boot-http-status-code-location/src/main/java/com/mimaraslan/controller/UserController.java
@@ -1,11 +1,17 @@
 package com.mimaraslan.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mimaraslan.model.User;
 import com.mimaraslan.service.UserService;
@@ -17,14 +23,35 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
-	@GetMapping("/user")
+	@GetMapping("/users")
 	public List<User> retrieveAllUsers(){
 		return userService.findAll();
 	}
 	
-	@GetMapping("/user/{id}")
+	@GetMapping("/users/{id}")
 	public User retrieveUser(@PathVariable int id){
 		return userService.findOne(id);
+	}
+	
+	
+	@PostMapping("/users/v1")
+	public User createUserV1(@RequestBody User user){
+		User savedUser = userService.save(user);
+		return savedUser;
+	}
+	
+	
+	@PostMapping("/users")
+	public ResponseEntity<Object> createUser(@RequestBody User user){
+		User savedUser = userService.save(user);
+		
+		URI location = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(savedUser.getId()).toUri();
+				
+		return ResponseEntity.created(location).build();
+	
 	}
 	
 }
