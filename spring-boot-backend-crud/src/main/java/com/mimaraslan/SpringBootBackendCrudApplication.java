4198b30d9468c1011d143658357b1551805ecddc
mimaraslan <mimaraslan@gmail.com>
2020-09-03 00:10:56 +0300

    Spring Boot Backend CRUD
    Spring Boot Backend CRUD

---
+++ spring-boot-backend-crud/src/main/java/com/mimaraslan/SpringBootBackendCrudApplication.java
@@ -0,0 +1,36 @@
+package com.mimaraslan;
+
+import com.mimaraslan.model.Employee;
+import com.mimaraslan.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBackendCrudApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBackendCrudApplication.class, args);
+	}
+
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		Employee employee = new Employee();
+		employee.setFirstName("FirstName1");
+		employee.setLastName("LastName1");
+		employee.setEmailId("EmailId1");
+		employeeRepository.save(employee);
+
+		Employee employee2 = new Employee();
+		employee2.setFirstName("FirstName2");
+		employee2.setLastName("LastName2");
+		employee2.setEmailId("EmailId2");
+		employeeRepository.save(employee2);
+
+	}
+}
--- spring-boot-backend-crud/src/main/java/com/mimaraslan/model/Employee.java
+++ spring-boot-backend-crud/src/main/java/com/mimaraslan/model/Employee.java
@@ -1,2 +1,31 @@
-package com.mimaraslan.model;public class Employee {
+package com.mimaraslan.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table (name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column (name = "first_name")
+    private String firstName;
+
+    @Column (name = "last_name")
+    private String lastName;
+
+    @Column (name = "email_id")
+    private String emailId;
+
 }
