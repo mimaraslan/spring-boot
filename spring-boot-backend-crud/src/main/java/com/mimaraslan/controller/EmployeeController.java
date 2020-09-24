d83dfb9da910b207d34c9e8c5791943d8fcb1707
mimaraslan <mimaraslan@gmail.com>
2020-09-24 03:42:09 +0300

    Spring Boot Backend CRUD
    Add new employee

--- spring-boot-backend-crud/src/main/java/com/mimaraslan/controller/EmployeeController.java
+++ spring-boot-backend-crud/src/main/java/com/mimaraslan/controller/EmployeeController.java
@@ -3,10 +3,7 @@ package com.mimaraslan.controller;
 import com.mimaraslan.model.Employee;
 import com.mimaraslan.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +18,10 @@ public class EmployeeController {
     @GetMapping
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
+    }
+
+    @PostMapping
+    public Employee createEmployee (@RequestBody Employee employee){
+        return employeeRepository.save(employee);
     }
 }
