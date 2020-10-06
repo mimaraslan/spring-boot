7c615d5d153c148a59f5def151ed89649acd2321
mimaraslan <mimaraslan@gmail.com>
2020-10-06 03:30:50 +0300

    Spring Boot Backend CRUD
    Delete

--- spring-boot-backend-crud/src/main/java/com/mimaraslan/controller/EmployeeController.java
+++ spring-boot-backend-crud/src/main/java/com/mimaraslan/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import com.mimaraslan.exception.ResourceNotFoundException;
 import com.mimaraslan.model.Employee;
 import com.mimaraslan.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -45,5 +46,16 @@ public class EmployeeController {
         employeeRepository.save(updateEmployee);
 
         return ResponseEntity.ok(updateEmployee);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<HttpStatus> deleteEmployee(@PathVariable long id){
+
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id: " + id));
+
+        employeeRepository.delete(employee);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
     }
 }
