6af789cd112d2faa2a47e1d9e18212f62d42635f
mimaraslan <mimaraslan@gmail.com>
2020-10-14 22:49:05 +0300

    Spring Boot Backend CRUD
    Spring Boot Backend CRUD

--- spring-boot-backend-crud/src/main/java/com/mimaraslan/controller/EmployeeController.java
+++ spring-boot-backend-crud/src/main/java/com/mimaraslan/controller/EmployeeController.java
@@ -1,5 +1,6 @@
 package com.mimaraslan.controller;
 
+import com.mimaraslan.config.DBConfig;
 import com.mimaraslan.exception.ResourceNotFoundException;
 import com.mimaraslan.model.Employee;
 import com.mimaraslan.repository.EmployeeRepository;
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(origins = "http://localhost:3000/")
 @RestController
@@ -18,23 +20,52 @@ public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
 
+    @Autowired
+    private DBConfig dbConfig;
+
+    // LIST
     @GetMapping
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
     }
 
+    @GetMapping("/all")
+    public ResponseEntity<List<Employee>> getAllEmployees2() {
+        return ResponseEntity.ok(employeeRepository.findAll());
+    }
+
+    // ADD
     @PostMapping
     public Employee createEmployee (@RequestBody Employee employee){
         return employeeRepository.save(employee);
     }
 
+
+    @PostMapping("/add")
+    public ResponseEntity<Employee> createEmployee2 (@RequestBody Employee employee){
+        return ResponseEntity.ok(employeeRepository.save(employee));
+    }
+
+    // LIST ID
     @GetMapping("{id}")
     public ResponseEntity<Employee> getEmployeeId(@PathVariable long id){
+
         Employee employee = employeeRepository.findById(id)
                 .orElseThrow(()-> new ResourceNotFoundException("Employee not exist with id: "+ id));
         return ResponseEntity.ok(employee);
     }
 
+    @GetMapping("/get/{id}")
+    public ResponseEntity<Optional<Employee>> getEmployeeId2(@PathVariable long id){
+       Optional<Employee> empObj = employeeRepository.findById(id);
+        if(empObj.isPresent()){
+            return ResponseEntity.ok(empObj);
+        }else{
+            throw new ResourceNotFoundException("Employee not exist with id: "+ id);
+        }
+    }
+
+    // UPDATE
     @PutMapping("{id}")
     public ResponseEntity<Employee> updateEmployee(@PathVariable long id, @RequestBody Employee employee){
         Employee updateEmployee = employeeRepository.findById(id)
@@ -48,6 +79,7 @@ public class EmployeeController {
         return ResponseEntity.ok(updateEmployee);
     }
 
+    // DELETE
     @DeleteMapping("{id}")
     public ResponseEntity<HttpStatus> deleteEmployee(@PathVariable long id){
 
