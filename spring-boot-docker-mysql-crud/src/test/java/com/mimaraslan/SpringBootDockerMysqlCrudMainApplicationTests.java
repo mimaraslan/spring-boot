2246378331eff64dfb58511cef5902c801a865a2
mimaraslan <mimaraslan@gmail.com>
2020-10-28 02:47:46 +0300

    Spring Boot Docker
    Spring Boot Docker

--- spring-boot-docker-mysql-crud/src/test/java/com/mimaraslan/SpringBootDockerMysqlCrudMainApplicationTests.java
+++ spring-boot-docker-mysql-crud/src/test/java/com/mimaraslan/SpringBootDockerMysqlCrudMainApplicationTests.java
@@ -1,12 +1,7 @@
 package com.mimaraslan;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class SpringBootDockerMysqlCrudMainApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
