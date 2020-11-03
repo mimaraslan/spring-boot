4a612fb88b062f67376ab79031df44e646eae1a1
mimaraslan <mimaraslan@gmail.com>
2020-11-04 01:07:02 +0300

    HATEOAS (Hypermedia as the Engine of Application State)
    HATEOAS (Hypermedia as the Engine of Application State)

--- spring-boot-jpa-rest-hateoas/src/main/java/com/mimaraslan/repository/EmployeeRepository.java
+++ spring-boot-jpa-rest-hateoas/src/main/java/com/mimaraslan/repository/EmployeeRepository.java
@@ -1,4 +1,4 @@
-package com.mimaraslan.resoruce;
+package com.mimaraslan.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.mimaraslan.model.Employee;
 
 @Repository
-public interface EmployeeResource extends JpaRepository<Employee, Long> {
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
 }
