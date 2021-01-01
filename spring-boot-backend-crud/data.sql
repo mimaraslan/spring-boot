596a6f440ae8c888807c85e673b6e1d56bb5be62
mimaraslan <mimaraslan@gmail.com>
2021-01-01 22:01:45 +0300

    Spring Boot Backend CRUD
    Spring Boot Backend CRUD

---
+++ spring-boot-backend-crud/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO `mydemodb`.`employees` (`first_name`, `last_name`,`email_id`,`birthday`)
+VALUES ("Yulya", "Sveta", "test1@mail.com", sysdate());
+
+INSERT INTO `mydemodb`.`employees` (`first_name`, `last_name`,`email_id`,`birthday`)
+VALUES ("Katerina", "Lena", "test2@mail.com", sysdate());
+
+INSERT INTO `mydemodb`.`employees` (`first_name`, `last_name`,`email_id`,`birthday`)
+VALUES ("Anna", "Raya", "test3@mail.com", sysdate());
+
---
+++ spring-boot-backend-crud/db.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS mydemodb DEFAULT CHARACTER SET utf8 COLLATE utf8_bin ;
+
+CREATE TABLE `mydemodb`.`employees` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `last_name` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `email_id` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `birthday` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
