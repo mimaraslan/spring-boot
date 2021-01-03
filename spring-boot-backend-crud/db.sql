fbd571b514600415d9e175930538120ee19a2e68
mimaraslan <mimaraslan@gmail.com>
2021-01-03 22:02:06 +0300

    Spring Boot Backend CRUD
    Spring Boot Backend CRUD

--- spring-boot-backend-crud/db.sql
+++ spring-boot-backend-crud/db.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE IF NOT EXISTS mydemodb DEFAULT CHARACTER SET utf8 COLLATE utf8_bin ;
 
+USE mydemodb;
+
 CREATE TABLE `mydemodb`.`employees` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `first_name` varchar(255) COLLATE utf8_bin DEFAULT NULL,
