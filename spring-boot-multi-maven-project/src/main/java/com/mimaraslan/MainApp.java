a930aa6644ce8d0a147d2f1acd15ede354ae8f78
mimaraslan <mimaraslan@gmail.com>
2021-06-05 19:30:13 +0300

    Multi Module Maven Project
    Multi Module Maven Project

--- spring-boot-multi-maven-project/src/main/java/com/mimaraslan/MainApp.java
+++ spring-boot-multi-maven-project/src/main/java/com/mimaraslan/MainApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LimitsServiceApplication {
+public class MainApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LimitsServiceApplication.class, args);
+		SpringApplication.run(SubApp1.class, args);
 	}
 
 }
--- spring-boot-multi-maven-project/src/main/java/com/mimaraslan/controller/Student.java
+++ spring-boot-multi-maven-project/src/main/java/com/mimaraslan/controller/Student.java
@@ -1,2 +1,19 @@
-package com.mimaraslan.controller;public class Student {
+package com.mimaraslan.controller;
+
+import com.mimaraslan.util.Utility;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/student")
+public class Student {
+
+    // http://localhost:8082/student/info/{studentName}
+    @GetMapping("/info/{studentName}")
+    public boolean getInfo (@PathVariable String studentName){
+        System.out.println(studentName);
+        return Utility.isNotNull(studentName);
+    }
 }
