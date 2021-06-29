3942f41b0a6589f0cf82d33116a10716b6fb83f1
mimaraslan <mimaraslan@gmail.com>
2021-06-29 21:50:20 +0300

    Multi Module Maven Project
    Multi Module Maven Project

--- spring-boot-multi-maven-project/src/main/java/com/mimaraslan/MainApp.java
+++ spring-boot-multi-maven-project/src/main/java/com/mimaraslan/MainApp.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MainApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SubApp1.class, args);
+		SpringApplication.run(SubProject1Application.class, args);
 	}
 
 }
--- spring-boot-multi-maven-project/src/main/java/com/mimaraslan/controller/Student.java
+++ spring-boot-multi-maven-project/src/main/java/com/mimaraslan/controller/Student.java
@@ -1,6 +1,6 @@
 package com.mimaraslan.controller;
 
-import com.mimaraslan.util.Utility;
+import com.mimaraslan.store.Utility;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
