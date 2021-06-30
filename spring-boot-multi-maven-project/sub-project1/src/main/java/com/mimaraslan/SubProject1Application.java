a844c6d8d6db0d586ebcfa266ff0f0888e17aae9
mimaraslan <mimaraslan@gmail.com>
2021-06-30 21:50:35 +0300

    Multi Module Maven Project
    Multi Module Maven Project

--- spring-boot-multi-maven-project/sub-project1/src/main/java/com/mimaraslan/SubProject1Application.java
+++ spring-boot-multi-maven-project/sub-project1/src/main/java/com/mimaraslan/SubProject1Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SubApp1 {
+public class SubProject1Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SubApp1.class, args);
+        SpringApplication.run(SubProject1Application.class, args);
     }
 
 }
