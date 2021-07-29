634e41f9ebfa907fbbf3977ce51369a3db0428f0
mimaraslan <mimaraslan@gmail.com>
2021-07-29 22:50:25 +0300

    HAL - Hypertext Application Language
    HAL - Hypertext Application Language

---
+++ spring-boot-rest-hal/src/test/java/com/mimaraslan/SpringBootBackendCrudApplicationTests.java
@@ -0,0 +1,68 @@
+package com.mimaraslan;
+
+import com.mimaraslan.model.Employee;
+import com.mimaraslan.repository.EmployeeRepository;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import com.mimaraslan.service.EmployeeService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class SpringBootBackendCrudApplicationTests {
+
+	@Autowired
+	private EmployeeService service;
+
+	@Autowired
+	private EmployeeRepository repository;
+
+
+	@Test
+	public void getUsersTest() {
+		when(repository.findAll()).thenReturn(Stream
+				.of(
+						new Employee(1, "A", "B", "C", null, null),
+						new Employee(2, "A", "B", "C", null, null)
+				)
+				.collect(Collectors.toList()));
+		assertEquals(2, service.getEmployees().size());
+	}
+
+	/*
+	@Test
+	public void getEmployeeByIdTest() {
+		int numId = 1;
+		when(repository.findById(numId))
+				.thenReturn(Stream.of(new Employee(2, "A", "66", "AAA")).collect(Collectors.toList()));
+		assertEquals(1, service.getEmployeeById(numId).size());
+	}
+	*/
+
+	@Test
+	public void saveUserTest() {
+		Employee user = new Employee(2, "A", "B", "C", null, null);
+		when(repository.save(user)).thenReturn(user);
+		assertEquals(user, service.addEmployee(user));
+	}
+
+	@Test
+	public void deleteUserTest() {
+		Employee user = new Employee(2, "A", "B", "C",null, null);
+		service.deleteEmployee(user);
+		verify(repository, times(1)).delete(user);
+	}
+
+}
