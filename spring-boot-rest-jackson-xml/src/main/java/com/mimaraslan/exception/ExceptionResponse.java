e5a2eb319914e5b2431c66bb19cf6011c4c6e2b9
mimaraslan <mimaraslan@gmail.com>
2021-08-26 22:54:54 +0300

    Jackson Data Format XML
    Jackson Data Format XML

---
+++ spring-boot-rest-jackson-xml/src/main/java/com/mimaraslan/exception/ExceptionResponse.java
@@ -0,0 +1,42 @@
+package com.mimaraslan.exception;
+
+import java.util.Date;
+
+public class ExceptionResponse {
+
+	private Date timestamp;
+	private String message;
+	private String detail;
+
+	public ExceptionResponse(Date timestamp, String message, String details) {
+		super();
+		this.timestamp = timestamp;
+		this.message = message;
+		this.detail = details;
+	}
+
+	public Date getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getDetail() {
+		return detail;
+	}
+
+	public void setDetails(String details) {
+		this.detail = details;
+	}
+
+}
