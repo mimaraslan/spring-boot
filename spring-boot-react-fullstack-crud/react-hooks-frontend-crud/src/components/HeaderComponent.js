32424f8a7b7b49039d642cd2d08ba53c3f10f070
mimaraslan <mimaraslan@gmail.com>
2022-05-18 00:06:10 +0300

    Spring Boot
    Spring Boot

---
+++ spring-boot-react-fullstack-crud/react-hooks-frontend-crud/src/components/HeaderComponent.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const HeaderComponent = () => {
+  return (
+    <div>
+        <header>
+            <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+                <a href="http://localhost:3000/" className="navbar-brand">
+                    Employee Managemend Application
+                </a>
+            </nav>
+        </header>
+    </div>
+  )
+}
+
+export default HeaderComponent
