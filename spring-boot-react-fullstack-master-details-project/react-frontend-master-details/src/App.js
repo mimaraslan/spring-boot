94f6ca32d29ad682e7e4d0613e3d948248d4dc26
mimaraslan <mimaraslan@gmail.com>
2022-05-29 21:34:29 +0300

    Master Details Tables
    Master Details Tables

---
+++ spring-boot-react-fullstack-master-details-project/react-frontend-master-details/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello ReactJS
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
