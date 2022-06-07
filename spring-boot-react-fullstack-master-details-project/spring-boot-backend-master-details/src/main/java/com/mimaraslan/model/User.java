3fa26226f55536c73d69386b6d225a0e958c7034
mimaraslan <mimaraslan@gmail.com>
2022-06-07 03:18:18 +0300

    Master Details Tables
    Master Details Tables

--- spring-boot-react-fullstack-master-details-project/spring-boot-backend-master-details/src/main/java/com/mimaraslan/model/User.java
+++ spring-boot-react-fullstack-master-details-project/spring-boot-backend-master-details/src/main/java/com/mimaraslan/model/User.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 public class User {
 
     @Id
-    //@GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
     String userName;
--- spring-boot-react-fullstack-master-details-project/spring-boot-backend-master-details/src/main/java/com/mimaraslan/service/PostService.java
+++ spring-boot-react-fullstack-master-details-project/spring-boot-backend-master-details/src/main/java/com/mimaraslan/service/PostService.java
@@ -1,8 +1,11 @@
 package com.mimaraslan.service;
 
 import com.mimaraslan.model.Post;
+import com.mimaraslan.model.User;
+import com.mimaraslan.model.request.PostCreateRequest;
 import com.mimaraslan.repository.PostRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,11 +14,12 @@ import java.util.Optional;
 public class PostService {
 
     private PostRepository postRepository;
+    private UserService userService;
 
-    public PostService(PostRepository postRepository) {
+    public PostService(PostRepository postRepository, UserService userService) {
         this.postRepository = postRepository;
+        this.userService = userService;
     }
-
 
     public List<Post> getAllPosts(Optional<Long> userId) {
         if(userId.isPresent()){
@@ -24,11 +28,44 @@ public class PostService {
         return postRepository.findAll();
     }
 
-    public Post getOnePost(Long postId) {
+    public Post getOnePostById(Long postId) {
         return postRepository.findById(postId).orElse(null);
     }
 
-    public Post createOnePost(Post newPost) {
+    public Post createOnePost1(Post newPost) {
         return postRepository.save(newPost);
     }
+
+    public Post createOnePost2(PostCreateRequest newPostRequest) {
+     User userObj =  userService.getOneUser(newPostRequest.getUserId());
+        if (userObj == null)
+            return null;
+
+        Post postObj = new Post();
+        postObj.setId(newPostRequest.getId());
+        postObj.setTitle(newPostRequest.getTitle());
+        postObj.setText(newPostRequest.getText());
+        postObj.setUser(userObj);
+        return postRepository.save(postObj);
+    }
+
+    public Post updateOnePostById(Long postId,  Post updatePost) {
+        Optional<Post> postObj= postRepository.findById(postId);
+        if (postObj.isPresent()) {
+            Post toUpdate = postObj.get();
+            toUpdate.setText(updatePost.getText());
+            toUpdate.setTitle(updatePost.getTitle());
+            postRepository.save(toUpdate);
+            return toUpdate;
+        } else {
+            return null;
+        }    }
+
+    public String deleteOnePostById(Long postId) {
+        if (postRepository.findById(postId).isPresent()) {
+            postRepository.deleteById(postId);
+            return "Deleted one user";
+        } else {
+            return null;
+        }    }
 }
