6cd7e75792595f3da9b6d7bf90db797df0a15ff7
mimaraslan <mimaraslan@gmail.com>
2022-07-03 20:43:51 +0300

    Spring Boot
    Spring Boot

--- spring-boot-active-profile/src/main/java/com/mimaraslan/controller/SpringProfilesController.java
+++ spring-boot-active-profile/src/main/java/com/mimaraslan/controller/SpringProfilesController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SpringProfilesController {
 
-	@Value("${property.myval}")
+	@Value("${property.myVal}")
 	private String profile;
 
 	@Autowired
