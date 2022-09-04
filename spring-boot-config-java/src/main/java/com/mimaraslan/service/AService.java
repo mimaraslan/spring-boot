b394c6ab0c119f70443d3a8612349f256a495fbc
mimaraslan <mimaraslan@gmail.com>
2022-09-05 00:35:38 +0300

    Spring Boot
    Spring Boot

--- spring-boot-config-java/src/main/java/com/mimaraslan/service/AService.java
+++ spring-boot-config-java/src/main/java/com/mimaraslan/service/AService.java
@@ -1,9 +1,9 @@
 package com.mimaraslan.service;
 
-public class AService implements SelectService{
+public class AService implements SelectService {
 
-	public void sendMsg(String message) {
-		System.out.println("Service A : " + message);
-	}
+    public void sendMsg(String message) {
+        System.out.println("Service A : " + message);
+    }
 
 }
--- spring-boot-config-java/src/main/java/com/mimaraslan/service/CService.java
+++ spring-boot-config-java/src/main/java/com/mimaraslan/service/CService.java
@@ -1,9 +1,9 @@
 package com.mimaraslan.service;
 
-public class CService implements SelectService{
+public class CService implements SelectService {
 
-	public void sendMsg(String message) {
-		System.out.println("Service C : " + message);
-	}
+    public void sendMsg(String message) {
+        System.out.println("Service C : " + message);
+    }
 
 }
