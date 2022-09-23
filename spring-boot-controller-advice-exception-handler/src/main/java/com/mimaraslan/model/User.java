ec3f852d2ce1a93b3328d0f342cf8c7cb21ba722
mimaraslan <mimaraslan@gmail.com>
2022-09-24 01:15:07 +0300

    Spring Boot
    Spring Boot

--- spring-boot-controller-advice-exception-handler/src/main/java/com/mimaraslan/model/User.java
+++ spring-boot-controller-advice-exception-handler/src/main/java/com/mimaraslan/model/User.java
@@ -4,46 +4,46 @@ import java.util.Date;
 
 public class User {
 
-	private Integer id;
+    private Integer id;
 
-	private String name;
+    private String name;
 
-	private Date birthDate;
+    private Date birthDate;
 
-	public User(Integer id, String name, Date birthDate) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.birthDate = birthDate;
-	}
+    public User(Integer id, String name, Date birthDate) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.birthDate = birthDate;
+    }
 
-	public Integer getId() {
-		return id;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public Date getBirthDate() {
-		return birthDate;
-	}
+    public Date getBirthDate() {
+        return birthDate;
+    }
 
-	public void setBirthDate(Date birthDate) {
-		this.birthDate = birthDate;
-	}
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
 
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", name=" + name + ", birthDate=" + birthDate + "]";
-	}
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", name=" + name + ", birthDate=" + birthDate + "]";
+    }
 
 }
