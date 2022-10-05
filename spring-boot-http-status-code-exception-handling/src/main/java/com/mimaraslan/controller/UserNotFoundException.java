8bb7f1b1db943a83be39a0e59b0f4a15ae53f3b1
mimaraslan <mimaraslan@gmail.com>
2022-10-06 01:16:26 +0300

    Spring Boot
    Spring Boot

--- spring-boot-http-status-code-exception-handling/src/main/java/com/mimaraslan/controller/UserNotFoundException.java
+++ spring-boot-http-status-code-exception-handling/src/main/java/com/mimaraslan/controller/UserNotFoundException.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public UserNotFoundException(String message) {
-		super(message);
-	}
+    public UserNotFoundException(String message) {
+        super(message);
+    }
 
 }
