41993fdb1b568b4c52ad5a1d42a697ef2ef43d8d
mimaraslan <mimaraslan@gmail.com>
2022-10-27 01:19:07 +0300

    Spring Boot
    Spring Boot

--- spring-boot-jpa-crud-rest/src/test/java/com/mimaraslan/MainApplicationTests.java
+++ spring-boot-jpa-crud-rest/src/test/java/com/mimaraslan/MainApplicationTests.java
@@ -1,13 +1,14 @@
 package com.mimaraslan;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBootDockerRestApplicationTests {
+public class MainApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
