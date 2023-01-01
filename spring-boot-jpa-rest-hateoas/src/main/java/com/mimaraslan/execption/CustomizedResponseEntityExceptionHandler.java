2896562c33adbd94f2d5acaafd37f9741ba27cc6
mimaraslan <mimaraslan@gmail.com>
2023-01-02 01:24:53 +0300

    Spring Boot
    Spring Boot

--- spring-boot-jpa-rest-hateoas/src/main/java/com/mimaraslan/execption/CustomizedResponseEntityExceptionHandler.java
+++ spring-boot-jpa-rest-hateoas/src/main/java/com/mimaraslan/execption/CustomizedResponseEntityExceptionHandler.java
@@ -2,7 +2,6 @@ package com.mimaraslan.execption;
 
 import java.util.Date;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
