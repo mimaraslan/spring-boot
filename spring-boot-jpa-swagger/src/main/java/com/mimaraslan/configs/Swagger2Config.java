00877bd991d75b3f959f5e0676b1817783b8b26e
mimaraslan <mimaraslan@gmail.com>
2023-01-15 02:30:06 +0300

    Spring Boot
    Spring Boot

--- spring-boot-jpa-swagger/src/main/java/com/mimaraslan/configs/Swagger2Config.java
+++ spring-boot-jpa-swagger/src/main/java/com/mimaraslan/configs/Swagger2Config.java
@@ -14,24 +14,24 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class Swagger2Config {
-	
-	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors
-				.basePackage("com.mimaraslan.controllers"))
-				.paths(PathSelectors.regex("/.*"))
-				.build()
-				.apiInfo(apiEndPointsInfo());
-	}
 
-	private ApiInfo apiEndPointsInfo() {
-		return new ApiInfoBuilder().title("Spring Boot REST API")
-				.description("Senior Java Developer, Computer Engineer")
-				.contact(new Contact("Mimar Aslan", "http://www.mimaraslan.com", "mimar.aslan@gmail.com"))
-				.license("Apache 2.0")
-				.licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
-				.version("1.0.0")
-				.build();
-	}
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors
+                        .basePackage("com.mimaraslan.controllers"))
+                .paths(PathSelectors.regex("/.*"))
+                .build()
+                .apiInfo(apiEndPointsInfo());
+    }
+
+    private ApiInfo apiEndPointsInfo() {
+        return new ApiInfoBuilder().title("Spring Boot REST API")
+                .description("Senior Java Developer, Computer Engineer")
+                .contact(new Contact("Mimar Aslan", "http://www.mimaraslan.com", "mimar.aslan@gmail.com"))
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .version("1.0.0")
+                .build();
+    }
 }
--- spring-boot-jpa-swagger/src/main/java/com/mimaraslan/exceptions/ErrorDetails.java
+++ spring-boot-jpa-swagger/src/main/java/com/mimaraslan/exceptions/ErrorDetails.java
@@ -3,26 +3,26 @@ package com.mimaraslan.exceptions;
 import java.util.Date;
 
 public class ErrorDetails {
-	private Date timestamp;
-	private String message;
-	private String details;
+    private Date timestamp;
+    private String message;
+    private String details;
 
-	public ErrorDetails(Date timestamp, String message, String details) {
-		super();
-		this.timestamp = timestamp;
-		this.message = message;
-		this.details = details;
-	}
+    public ErrorDetails(Date timestamp, String message, String details) {
+        super();
+        this.timestamp = timestamp;
+        this.message = message;
+        this.details = details;
+    }
 
-	public Date getTimestamp() {
-		return timestamp;
-	}
+    public Date getTimestamp() {
+        return timestamp;
+    }
 
-	public String getMessage() {
-		return message;
-	}
+    public String getMessage() {
+        return message;
+    }
 
-	public String getDetails() {
-		return details;
-	}
+    public String getDetails() {
+        return details;
+    }
 }
