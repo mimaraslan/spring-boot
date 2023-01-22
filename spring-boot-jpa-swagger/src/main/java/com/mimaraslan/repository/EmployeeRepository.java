5df4489bc55985342049936a5dd02b51ca3f55d1
mimaraslan <mimaraslan@gmail.com>
2023-01-23 02:30:47 +0300

    Spring Boot
    Spring Boot

--- spring-boot-jpa-swagger/src/main/java/com/mimaraslan/repository/EmployeeRepository.java
+++ spring-boot-jpa-swagger/src/main/java/com/mimaraslan/repository/EmployeeRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.mimaraslan.model.Employee;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
 }
