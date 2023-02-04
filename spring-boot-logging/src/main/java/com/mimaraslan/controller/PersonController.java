486f0bbc4c5c79eefbb63652e9e506e62692ec59
mimaraslan <mimaraslan@gmail.com>
2023-02-05 02:31:56 +0300

    Spring Boot
    Spring Boot

--- spring-boot-logging/src/main/java/com/mimaraslan/controller/PersonController.java
+++ spring-boot-logging/src/main/java/com/mimaraslan/controller/PersonController.java
@@ -1,6 +1,7 @@
 package com.mimaraslan.controller;
 
 import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,21 +13,21 @@ import com.mimaraslan.service.PersonService;
 @RestController
 public class PersonController {
 
-	private static final Logger logger = LoggerFactory.getLogger(PersonController.class);
+    private static final Logger logger = LoggerFactory.getLogger(PersonController.class);
 
-	@Autowired
-	private PersonService personService;
+    @Autowired
+    private PersonService personService;
 
-	@GetMapping("/person")
-	public List<Person> getPersons() {
+    @GetMapping("/person")
+    public List<Person> getPersons() {
 
-		String className = this.getClass().getSimpleName();
-		logger.trace("A TRACE Message: " + className);
-		logger.debug("A DEBUG Message: " + className);
-		logger.info("An INFO Message: " + className);
-		logger.warn("A WARN Message: " + className);
-		logger.error("An ERROR Message: " + className);
+        String className = this.getClass().getSimpleName();
+        logger.trace("A TRACE Message: " + className);
+        logger.debug("A DEBUG Message: " + className);
+        logger.info("An INFO Message: " + className);
+        logger.warn("A WARN Message: " + className);
+        logger.error("An ERROR Message: " + className);
 
-		return personService.getPersons();
-	}
+        return personService.getPersons();
+    }
 }
