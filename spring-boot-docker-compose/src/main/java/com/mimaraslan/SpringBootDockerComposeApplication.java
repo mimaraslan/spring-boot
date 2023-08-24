b1fc7670e754cf471ae10e5453622f8d92fd2a99
mimaraslan <mimaraslan@gmail.com>
2023-08-25 01:18:57 +0300

    Docker Compose
    Docker Compose

---
+++ spring-boot-docker-compose/src/main/java/com/mimaraslan/SpringBootDockerComposeApplication.java
@@ -0,0 +1,13 @@
+package com.mimaraslan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerComposeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDockerComposeApplication.class, args);
+    }
+
+}
