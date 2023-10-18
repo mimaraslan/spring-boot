4444830d40996534414e2a74fec950621d01474f
Mimar Aslan <mimaraslan@gmail.com>
2023-10-17 21:07:44 -0500

    Spring Boot
    Spring Boot

--- spring-boot-lombok-junit-mockito/src/main/java/com/mimaraslan/service/EmployeeService.java
+++ spring-boot-lombok-junit-mockito/src/main/java/com/mimaraslan/service/EmployeeService.java
@@ -1,10 +1,13 @@
 package com.mimaraslan.service;
 
 import com.mimaraslan.model.Employee;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+
+@Service
 public interface EmployeeService {
     Employee saveEmployee(Employee employee) throws Exception;
 
