f873e0fa293eb088fbe80585306b8e88a5406ed2
Mimar Aslan <mimaraslan@gmail.com>
2024-12-22 19:12:11 +0300

    Redis
    Redis

--- spring-boot-redis/src/main/java/com/mimaraslan/_004_RedisKeyJava.java
+++ spring-boot-redis/src/main/java/com/mimaraslan/_004_RedisKeyJava.java
@@ -8,13 +8,9 @@ import redis.clients.jedis.Jedis;
 public class _004_RedisKeyJava {
 	public static void main(String[] args) {
 
-		// Connecting to Redis server on localhost
 		Jedis jedis = new Jedis("localhost");
 		System.out.println("Connection to server sucessfully");
-		// store data in redis list
 
-		// TODO NOT RUNNING
-		// Get the stored data and print it
 		Set<String> keys = jedis.keys("*");
 		keys.forEach(System.out::println);
 
